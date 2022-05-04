f4b8c1797ff2fff687b8c10d6120acf417087fd4
Paulo Pinheiro <pauloprap57@gmail.com>
2022-05-04 03:33:23 -0300

    update

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -4,7 +4,7 @@ import { styles } from "./styles";
 
 export function GuildIcon() {
   const uri =
-    "https://e7.pngegg.com/pngimages/672/63/png-clipart-discord-computer-icons-online-chat-cool-discord-icon-logo-smiley.png";
+    "https://gamerssuffice.com/wp-content/uploads/2019/11/How-to-add-bots-to-discord-500x405.jpg";
 
   return <Image source={{ uri }} style={styles.image} resizeMode="cover" />;
 }
